d02599dcca0fe7f02a5f2cf7b902d73201a5880b
hope <hope@yeahka.com>
2023-01-06 18:13:09 +0800

    add

---
+++ pkg/river/river1.go
@@ -0,0 +1,9 @@
+package river
+
+import (
+	"fmt"
+)
+
+func Myfunc2() {
+	fmt.Println("river river1")
+}
---
+++ pkg/utils/transfer.go
@@ -0,0 +1,9 @@
+package utils
+
+import (
+	"fmt"
+)
+
+func Myfunc1() {
+	fmt.Println("utils transfer")
+}
